8ac5fc36a81cc8ff4e0a7b9202f4a7a3985250a4
btaba <7320238+btaba@users.noreply.github.com>
2025-04-14 15:36:02 -0700

    Shim for MJX-Warp - fix.

--- mjx/mujoco/mjx/_src/smooth.py
+++ mjx/mujoco/mjx/_src/smooth.py
@@ -38,66 +38,195 @@ import mujoco_warp as mjwarp
 from warp.jax_experimental.ffi import jax_callable
 
 
+
+# # SINGLE CALL
+# import jax
+# import jax.numpy as jp
+# import warp as wp
+# from warp.jax_experimental.ffi import jax_callable
+# @wp.kernel
+# def scale_kernel(a: wp.array(dtype=float), s: float, output: wp.array(dtype=float)):
+#     tid = wp.tid()
+#     output[tid] = a[tid] * s
+
+# def example_func(
+#     a: wp.array(dtype=float),
+#     s: float,
+#     c: wp.array(dtype=float),
+# ):
+#   print(a.shape)
+#   wp.launch(scale_kernel, dim=a.shape, inputs=[a, s], outputs=[c])
+
+
+# def jax_func(a: jax.Array, s: float):
+#   jf = jax_callable(example_func, num_outputs=1)
+#   return jf(a, s)
+
+# c = jax.jit(jax_func, static_argnums=(1,))(jp.ones(10), 2)
+# c = jax.jit(jax.vmap(jax_func, in_axes=(0, None)), static_argnums=(1,))(jp.ones((10, 10)), 2)
+
+
+# # VMAP CALL
+# import jax
+# import jax.numpy as jp
+# import warp as wp
+# from warp.jax_experimental.ffi import jax_callable
+
+# @wp.kernel
+# def scale_kernel(a: wp.array2d(dtype=float), s: float, output: wp.array2d(dtype=float)):
+#     wid, tid = wp.tid()
+#     output[wid, tid] = a[wid, tid] * s
+
+# def example_func(
+#     a: wp.array2d(dtype=float),
+#     s: float,
+#     c: wp.array2d(dtype=float),
+# ):
+#   wp.launch(scale_kernel, dim=a.shape, inputs=[a, s], outputs=[c])
+
+# def jax_func(a: jax.Array, s: float):
+#   jf = jax_callable(example_func, num_outputs=1, vmap_method="broadcast_all")
+#   return jf(a, s)
+
+# c = jax.jit(jax_func, static_argnums=(1,))(jp.ones((10, 10)), 2)  # works
+# c = jax.jit(jax.vmap(jax_func, in_axes=(0, None)), static_argnums=(1,))(jp.ones((10, 10, 10)), 2)  # fails
+
+
+# def jax_func(a: jax.Array, s: float):
+#   print('JAX', a)
+#   jf = jax_callable(example_func, num_outputs=1, vmap_method="broadcast_all")
+#   return jf(a, s)
+
+
+
+# jax_mjwarp_step = jax_callable(
+#   mjwarp_step,
+#   num_outputs=8,
+#   output_dims={
+#     "qpos_out": (NWORLD, mjm.nq),
+#     "qvel_out": (NWORLD, mjm.nv),
+#     "xpos_out": (NWORLD, mjm.nbody, 3),
+#     "xmat_out": (NWORLD, mjm.nbody, 3, 3),
+#     "qacc_warmstart_out": (NWORLD, mjm.nv),
+#     "subtree_com_out": (NWORLD, mjm.nbody, 3),
+#     "cvel_out": (NWORLD, mjm.nbody, 6),
+#     "site_xpos_out": (NWORLD, mjm.nsite, 3),
+#   },
+# )
+
+# # the functions below allow us to call MJWarp step inside jax vmap:
+
+
+# @jax.custom_batching.custom_vmap
+# def step(d: mjx.Data):
+#   return d
+
+
+# @step.def_vmap
+# def step_vmap_rule(axis_size, in_batched, d: mjx.Data):
+#   if in_batched[0].ctrl:
+#     assert d.ctrl.shape[0] == axis_size
+#   else:
+#     d = d.replace(ctrl=jp.tile(d.ctrl, (axis_size, 1)))
+#   params = {f.name: None for f in dataclasses.fields(mjx.Data)}
+#   params["ctrl"] = True
+#   params["qpos"] = True
+#   params["qvel"] = True
+#   params["xpos"] = True
+#   params["xmat"] = True
+#   params["qacc_warmstart"] = True
+#   params["subtree_com"] = True
+#   params["cvel"] = True
+#   params["site_xpos"] = True
+#   out_batched = mjx.Data(**params)
+
+#   qpos, qvel, xpos, xmat, qacc_warmstart, subtree_com, cvel, site_xpos = (
+#     jax_mjwarp_step(d.ctrl, d.qpos, d.qvel, d.qacc_warmstart)
+#   )
+#   d = d.replace(
+#     qpos=qpos,
+#     qvel=qvel,
+#     xpos=xpos,
+#     xmat=xmat,
+#     qacc_warmstart=qacc_warmstart,
+#     subtree_com=subtree_com,
+#     cvel=cvel,
+#     site_xpos=site_xpos,
+#   )
+#   return d, out_batched
+
+
+# TODO(btaba): do not use custom_vmap, jax_callable FFI should work with vmap...
+def _kinematics_warp(m: Model, d: Data, nworld: int) -> Data:
+  mjwarp_kinematics_jax = jax_callable(
+    mjwarp._src.smooth.kinematics_,
+    num_outputs=11,
+    output_dims={
+      "xpos": (nworld, m.nbody, 3),
+      "xquat": (nworld, m.nbody, 4),
+      "xmat": (nworld, m.nbody, 3, 3),
+      "xipos": (nworld, m.nbody, 3),
+      "ximat": (nworld, m.nbody, 3, 3),
+      "xanchor": (nworld, m.njnt, 3),
+      "xaxis": (nworld, m.njnt, 3),
+      "geom_xpos": (nworld, m.ngeom, 3),
+      "geom_xmat": (nworld, m.ngeom, 3, 3),
+      "site_xpos": (nworld, m.nsite, 3),
+      "site_xmat": (nworld, m.nsite, 3, 3),
+    }
+  )
+  out = mjwarp_kinematics_jax(
+    wp.to_jax(m._blob.body_tree),
+    m.qpos0,
+    m.body_parentid,
+    m.body_jntadr,
+    m.body_jntnum,
+    m.body_pos,
+    m.body_quat,
+    m.body_ipos,
+    m.body_iquat,
+    wp.to_jax(m._blob.body_treeadr),
+    m.jnt_type,
+    m.jnt_qposadr,
+    m.jnt_pos,
+    m.jnt_axis,
+    m.geom_bodyid,
+    m.geom_pos,
+    m.geom_quat,
+    m.site_bodyid,
+    m.site_pos,
+    m.site_quat,
+    # Data Inputs
+    jp.expand_dims(d.qpos, axis=0),
+  )
+  d = d.replace(
+    xpos=out[0],
+    xquat=out[1],
+    xmat=out[2],
+    xipos=out[3],
+    ximat=out[4],
+    xanchor=out[5],
+    xaxis=out[6],
+    geom_xpos=out[7],
+    geom_xmat=out[8],
+    site_xpos=out[9],
+    site_xmat=out[10],
+  )
+  return d
+
+@jax.custom_batching.custom_vmap
+def kinematics_warp(m: Model, d: Data) -> Data:
+  return _kinematics_warp(m, d, 1)
+
+@kinematics_warp.def_vmap
+def kinematics_warp_vmap(axis_size, in_batched, m: Model, d: Data) -> Data:
+  return _kinematics_warp(m, d, axis_size)
+
+
 def kinematics(m: Model, d: Data) -> Data:
   """Converts position/velocity from generalized coordinates to maximal."""
   if m._backend_impl == BackendImpl.WARP:
-    mjwarp_kinematics_jax = jax_callable(
-      mjwarp._src.smooth.kinematics_,
-      num_outputs=11,
-      output_dims={
-        "xpos": (1, m.nbody,),
-        "xquat": (1, m.nbody,),
-        "xmat": (1, m.nbody, 3, 3),
-        "xipos": (1, m.nbody,),
-        "ximat": (1, m.nbody, 3, 3),
-        "xanchor": (1, m.njnt,),
-        "xaxis": (1, m.njnt,),
-        "geom_xpos": (1, m.ngeom,),
-        "geom_xmat": (1, m.ngeom, 3, 3),
-        "site_xpos": (1, m.nsite,),
-        "site_xmat": (1, m.nsite, 3, 3),
-      }
-    )
-    print(d.qpos, d.qpos.shape, d.xpos)
-    out = mjwarp_kinematics_jax(
-      wp.to_jax(m._blob.body_tree),
-      m.qpos0,
-      m.body_parentid,
-      m.body_jntadr,
-      m.body_jntnum,
-      m.body_pos,
-      m.body_quat,
-      m.body_ipos,
-      m.body_iquat,
-      wp.to_jax(m._blob.body_treeadr),
-      m.jnt_type,
-      m.jnt_qposadr,
-      m.jnt_pos,
-      m.jnt_axis,
-      m.geom_bodyid,
-      m.geom_pos,
-      m.geom_quat,
-      m.site_bodyid,
-      m.site_pos,
-      m.site_quat,
-      # Data Inputs
-      jp.expand_dims(d.qpos, axis=0),
-    )
-    d = d.replace(
-      xpos=out[0],
-      xquat=out[1],
-      xmat=out[2],
-      xipos=out[3],
-      ximat=out[4],
-      xanchor=out[5],
-      xaxis=out[6],
-      geom_xpos=out[7],
-      geom_xmat=out[8],
-      site_xpos=out[9],
-      site_xmat=out[10],
-    )
-    return d
-
+    return kinematics_warp(m, d)
 
   def fn(carry, jnt_typs, jnt_pos, jnt_axis, qpos, qpos0, pos, quat):
     # calculate joint anchors, axes, body pos and quat in global frame
